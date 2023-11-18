4fbb3c959f217bc967e423a1a13b030a49007abd
Ayush4434 <146090632+Ayush4434@users.noreply.github.com>
2023-11-18 17:23:07 +0530

    quick_sort.cpp

---
+++ quick_sort.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+class Solution
+{
+    public:
+    //Function to sort an array using quick sort algorithm.
+    void quickSort(int arr[], int low, int high)
+    {
+        if(low<high){
+        int pivot = partition(arr,low,high);
+        quickSort(arr,low,pivot-1);
+        quickSort(arr,pivot+1,high);
+    }
+    }
+    
+    public:
+    int partition (int arr[], int low, int high)
+    {
+       // Your code here
+       int pi = arr[high];
+       int j = low-1;
+       for(int i= low;i<high;i++)
+       {
+           if(arr[i]<pi)
+           {
+               j++;
+               swap(arr[i],arr[j]);
+           }
+       }
+       swap(arr[j+1],arr[high]);
+       return j+1;
+    }
+};
+int main()
+{
+    int arr[1000],n,T,i;
+    scanf("%d",&T);
+    while(T--){
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+      scanf("%d",&arr[i]);
+      Solution ob;
+    ob.quickSort(arr, 0, n-1);
+    printArray(arr, n);
+    }
+    return 0;
+}
